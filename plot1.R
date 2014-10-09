c3b4da8f0acb1ef3c4559c9a310e714b74c45705
Evgeniy Bliacher <Evgeniy.Bliacher@allscripts.com>
2014-10-09 06:49:12 +0300

    Assament 1

---
+++ plot1.R
@@ -0,0 +1,20 @@
+#Download and read raw data
+
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+data <- read.csv(unz(temp, "household_power_consumption.txt"), header = TRUE, sep = ";", na.strings="?")
+unlink(temp)
+
+
+
+#Go to the tidy data
+data <- within(data, { timestamp=paste(Date, Time) })
+data$timestamp <- strptime(data$timestamp, "%d/%m/%Y %H:%M:%S")
+
+relevantData <- subset(data, timestamp > strptime("31/01/2007 23:59:59", "%d/%m/%Y %H:%M:%S") & timestamp < strptime("03/02/2007 00:00:00", "%d/%m/%Y %H:%M:%S"))
+
+#Plot data
+par(mar=c(4,10,2,4))
+hist(relevantData$Global_active_power, col = 'red', ylim = range(0,1200,100), main = "Global Activity Power", xlab = "Global Activity Power(kilowatts)" )
+dev.copy(png, file="plot1.png")
+dev.off()
