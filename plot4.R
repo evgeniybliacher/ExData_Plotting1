477f1b25ed2474913ea79b3e2c2a320cede4e69d
Evgeniy Bliacher <Evgeniy.Bliacher@allscripts.com>
2014-10-14 22:42:13 +0300

    forgot to add
    forgot to add

---
+++ plot4.R
@@ -0,0 +1,29 @@
+#Download and read raw data
+
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+data <- read.csv(unz(temp, "household_power_consumption.txt"), header = TRUE, sep = ";", na.strings="?")
+unlink(temp)
+
+
+
+#Go to the tidy data
+data <- within(data, { timestamp=paste(Date, Time) })
+data$timestamp <- strptime(data$timestamp, "%d/%m/%Y %H:%M:%S")
+
+relevantData <- subset(data, timestamp > strptime("31/01/2007 23:59:59", "%d/%m/%Y %H:%M:%S") & timestamp < strptime("03/02/2007 00:00:00", "%d/%m/%Y %H:%M:%S"))
+
+#Plot data
+par(mar=c(4,10,2,4), mfrow=c(2,2))
+hist(relevantData$Global_active_power, col = 'red', ylim = range(0,1200,100), main = "Global Activity Power", xlab = "Global Activity Power(kilowatts)" )
+
+plot(relevantData$timestamp, relevantData$Voltage, , type='l',xlab="datetime", ylab = "Voltage")
+
+plot(relevantData$timestamp, relevantData$Sub_metering_1, type='l',xlab="", ylab = "Energy sub metering")
+lines(relevantData$timestamp, relevantData$Sub_metering_2, col='red', type='l', ylab = "Energy sub metering")
+lines(relevantData$timestamp, relevantData$Sub_metering_3, col='blue', type='l', ylab = "Energy sub metering")
+legend('topright',lty=c(1,1),lwd=c(2.5,2.5),cex=0.5, c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), col=c("black","red","blue"))
+
+plot(relevantData$timestamp, relevantData$Global_reactive_power, , type='l',xlab="datetime", ylab = "Global_reactive_power")
+dev.copy(png, file="plot4.png")
+dev.off()
